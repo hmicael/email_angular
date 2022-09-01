7c48fe3b6599fc835500fb4b649cf089739af9de
henintsoa <handriamahadimby@gmail.com>
2022-09-01 12:09:15 +0300

    edit html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from './../environments/environment';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'email_angular';
   apiUrl = environment.apiURL;
+  isLogged!: boolean;
+
+
+  constructor(
+    private authService: AuthService
+    ) { }
+
+    ngOnInit(): void {
+      this.isLogged = this.authService.isLogged()
+    }
 }
