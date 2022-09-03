649b5ba02bef984eef710d82a181c8dfea244aca
henintsoa <handriamahadimby@gmail.com>
2022-09-03 19:21:07 +0300

    edit indentation

--- src/app/core/interceptors/http-response.interceptor.ts
+++ src/app/core/interceptors/http-response.interceptor.ts
@@ -14,46 +14,46 @@ export class HttpResponseInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
-     .pipe(
-       retry(0),
-       catchError((error: HttpErrorResponse) => {
-         let errorMessage = '';
-         if (error.error instanceof ErrorEvent) {
-           // source code error
-           errorMessage = error.error.message;
-         } else {
-           // server-side error
-           errorMessage = error.message;
-           switch (error.status) {
-            case 400: {
-              const errorBody: ErrorBody[] = error.error;
-              for (const error of errorBody) {
-                this.notificationService.showError(error.message.replace(/"/gi, ""), 'Error');
+      .pipe(
+        retry(0),
+        catchError((error: HttpErrorResponse) => {
+          let errorMessage = '';
+          if (error.error instanceof ErrorEvent) {
+            // source code error
+            errorMessage = error.error.message;
+          } else {
+            // server-side error
+            errorMessage = error.message;
+            switch (error.status) {
+              case 400: {
+                const errorBody: ErrorBody[] = error.error;
+                for (const error of errorBody) {
+                  this.notificationService.showError(error.message.replace(/"/gi, ""), 'Error');
+                }
+                break;
               }
-              break;
+              case 403: {
+                const errorBody: ErrorBody = error.error;
+                this.notificationService.showError(errorBody.message, 'Error');
+                break;
+              }
+              case 404: {
+                this.router.navigateByUrl('/404-error');
+                break;
+              }
+              case 500: {
+                this.notificationService.showError('A server internal error occured', 'Error');
+                break;
+              }
+              default: {
+                this.notificationService.showError(error.message, 'Error');
+                break;
+              }
             }
-            case 403: {
-              const errorBody: ErrorBody = error.error;
-              this.notificationService.showError(errorBody.message, 'Error');
-              break;
-            }
-            case 404: {
-              this.router.navigateByUrl('/404-error');
-              break;
-            }
-            case 500: {
-              this.notificationService.showError('A server internal error occured', 'Error');
-              break;
-            }
-            default: {
-              this.notificationService.showError(error.message, 'Error');
-              break;
-            }
-           }
-         }
-         console.error(errorMessage);
-         return throwError(() => new Error(errorMessage));
-       })
+          }
+          console.error(errorMessage);
+          return throwError(() => new Error(errorMessage));
+        })
      )
   }
 }
